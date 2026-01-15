3e84de1e3a70871feec93e50e6a1d35a28dc7afb
Dusan Stepancic <dusanjstepancic@gmail.com>
2026-01-15 21:00:22 +0100

    Dodato koriscenje initialTokena

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -7,10 +7,12 @@ import axios from 'axios';
 
 axios.defaults.baseURL = "http://127.0.0.1:8000/";
 
+const initialToken = window.localStorage.getItem("auth_token");
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App initialToken={initialToken}/>
   </React.StrictMode>
 );
 
