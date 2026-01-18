ec216d0b2a6eebe46ce51bdc4e2ad47be5e38c48
Aleksandar Uzelac <aleksandar.uzelac13@gmail.com>
2026-01-18 21:52:22 +0100

    Napravljen NavLink

---
+++ react-frontend/src/komponente/NavLink.jsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+
+const NavLink = ({to,text})=>{
+    return(
+        <Link to={to} className="nav-link">{text}</Link>
+    );
+};
+export default NavLink;
